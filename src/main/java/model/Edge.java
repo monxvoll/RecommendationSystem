733405a2321e1f1d7246ad0a2b5b568ab559754d
monx.voll <stedy3348112688@gmail.com>
2024-12-07 12:29:01 -0500

    Representa una arista en el grafo, con un origen, destino y peso.

--- src/main/java/model/Edge.java
+++ src/main/java/model/Edge.java
@@ -7,9 +7,9 @@ import java.io.Serializable;
 // Clase Edge para representar relaciones entre nodos
 public class Edge implements Serializable {
     @SerializedName("source")
-    private Node source; //(Origen) nodo donde empieza la arista, es decir que representa el punto de partida donde empieza una interacción
+    private Node source; // (Origen) nodo donde empieza la arista, es decir que representa el punto de partida donde empieza una interacción
     @SerializedName("target")
-    private Node target; //(Destino) donde termina la arista, como tal es el punto final de una interacción
+    private Node target; // (Destino) donde termina la arista, como tal es el punto final de una interacción
     @SerializedName("weight")
     private int weight; // En este caso por ejemplo, una valoración
 
